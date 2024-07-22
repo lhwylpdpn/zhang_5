ab2df6854af77ae1ac3ec55c5d88958199a54b8b
lhwylp <58254451@qq.com>
2024-07-22 19:03:39 +0800

    Update req_test.py

--- req_test.py
+++ req_test.py
@@ -3,7 +3,7 @@ import os
 import json
 import hmac
 import datetime
-SECRET_KEY= 'tokenTest'
+SECRET_KEY= 'test'
 channel_id='1000'
 SECRET_KEY = SECRET_KEY + str(channel_id) + "_"
 print('SECRET_KEY:', SECRET_KEY)
@@ -18,7 +18,7 @@ def generate_token():
 def upload_image():
     t, ts = generate_token()
     res = requests.post('http://127.0.0.1:5000/uploadimage', files={'file': open('upload/test4.jpg', 'rb')},
-                        headers={'token': 'test', 'Request-Id': t,'channel_id':'1000','symbol_id':'test'})
+                        headers={'token': 'test', 'Request-Id': t,'Channel_id':'1000','symbol_id':'test'})
     print(res.json())
     #print(res)
 def get_history():
