fd233b991c797b2aa9a7dbe616a260dfc1b3ebb5
lhwylp <58254451@qq.com>
2024-08-18 22:30:52 +0800

    1

--- logic/graph_process.py
+++ logic/graph_process.py
@@ -13,7 +13,7 @@ from skimage.metrics import structural_similarity as ssim
 # 加载图片
 
 def test1():
-    image = cv2.imread('../test4.jpg', 0)
+    image = cv2.imread('biaozhun4.jpg', 0)
 
     # 对图片进行阈值处理
     _, thresh = cv2.threshold(image, 150, 255, cv2.THRESH_BINARY_INV)
@@ -69,8 +69,8 @@ def test1():
         cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
     # # 显示包含直线的图像
-    # cv2.imshow('lines', line_image)
-    # cv2.waitKey(0)
+    cv2.imshow('lines', line_image)
+    cv2.waitKey(0)
     lines = cv2.HoughLinesP(thresh, 1, np.pi/180, 50, minLineLength=30, maxLineGap=10)
 
     # 计算交点
@@ -102,50 +102,97 @@ def test1():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def test2():
-    import cv2
-    import numpy as np
-
+def grid_graph(img):
     # 读取图像
-    img = cv2.imread('../test4.jpg')
+    col=10
+    row=10
 
     # 转换为灰度图像
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    src_img0 = cv2.GaussianBlur(gray,(3,3),0)
+    src_img1 = cv2.bitwise_not(src_img0)
+    AdaptiveThreshold = cv2.adaptiveThreshold(src_img1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, -2)
 
-    # 应用二值化
-    _, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
+    horizontal = AdaptiveThreshold.copy()
+    vertical = AdaptiveThreshold.copy()
+    scale = 20
 
-    # 使用形态学操作连接字符
-    kernel = np.ones((10, 10), np.uint8)
-    connected_text = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel)
+    horizontalSize = int(horizontal.shape[1] / scale)
+    horizontalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (horizontalSize, 1))
+    horizontal = cv2.erode(horizontal, horizontalStructure)
+    horizontal = cv2.dilate(horizontal, horizontalStructure)
 
+    verticalsize = int(vertical.shape[1] / scale)
+    verticalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (1, verticalsize))
+    vertical = cv2.erode(vertical, verticalStructure, (-1, -1))
+    vertical = cv2.dilate(vertical, verticalStructure, (-1, -1))
+    mask = horizontal + vertical
     # 找到轮廓
-    contours, _ = cv2.findContours(connected_text, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    # 遍历所有轮廓
-    min_width = 0
-    max_width = 10000
-    min_height = 0
-    max_height = 10000
-    #清空文件夹..//tmp//
-    import os
-    for file in os.listdir('..//tmp//'):
-        os.remove('..//tmp//'+file)
+    #contours, _ = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    #打印图片的尺寸
+    x,y,w,h = 0,0,0,0
     for cnt in contours:
-        # 计算轮廓的外接矩形
+        t_x, t_y, t_w, t_h = cv2.boundingRect(cnt)
+        #过滤足够大的轮廓
+        if t_w > img.shape[1]/2 and  t_w<img.shape[1]*4/5  and  t_h > img.shape[0]/2 and  t_h<img.shape[0]*4/5:
+            x, y, w, h = t_x, t_y, t_w, t_h
+
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            break
+    gird_list=[]
+    for cnt in contours:
+        t_x, t_y, t_w, t_h = cv2.boundingRect(cnt)
+        #只要在 x,y,w,h的范围内的轮廓
+        if t_x>x and t_y>y and t_x+t_w<x+w and t_y+t_h<y+h:
+            #cv2.rectangle(img, (t_x, t_y), (t_x + t_w, t_y + t_h), (0, 255, 0), 2)
+            gird_list.append((t_x, t_y, t_w, t_h))
+    image_res = img.copy()
+    remove_list=[]
+    for x, y, w, h in gird_list:
+        if abs(w-h)>=5:
+            remove_list.append((x, y, w, h))
+    gird_list=[x for x in gird_list if x not in remove_list]
+    print(len(gird_list))
+    #对每个轮廓的 先y后x进行排序
+    gird_list=sorted(gird_list,key=lambda x:(x[1],x[0]))
 
 
-        # 切割图像
-        x, y, w, h = cv2.boundingRect(cnt)
+    #先检查是否有近似重叠的
+    #假设精度够，那么w和h是可以被平均的
+    min_x = min([x for x, y, w, h in gird_list])
+    min_y = min([y for x, y, w, h in gird_list])
+    max_x = max([x + w for x, y, w, h in gird_list])
+    max_y = max([y + h for x, y, w, h in gird_list])
+    w_avg=sum([w for x, y, w, h in gird_list])/len(gird_list)
+    h_avg=sum([h for x, y, w, h in gird_list])/len(gird_list)
+    cut_info={}
+    j=0 #行
+    i=0 #列
+    for _ in range(len(gird_list)):
+        if _==0: #第一行第一列
+            cut_info[(i,j)]=(gird_list[_][0],gird_list[_][1],gird_list[_][2],gird_list[_][3])
+            i+=1
+        else:
+            if gird_list[_][1]-gird_list[_-1][1]<h_avg*2/3:#还在同一行
+                cut_info[(i,j)]=(gird_list[_][0],gird_list[_][1],gird_list[_][2],gird_list[_][3])
+                i+=1
+            else:
+                j+=1
+                i=0
+                cut_info[(i,j)]=(gird_list[_][0],gird_list[_][1],gird_list[_][2],gird_list[_][3])
+                i+=1
+    print(len(list(cut_info.keys()))) #todo 后续这个地方可以做检查
 
-        margin = 0  # 设置边距
-        roi = img[y + margin:y + h - margin, x + margin:x + w - margin]
-        if min_width <= w <= max_width and min_height <= h <= max_height:
-            cv2.imwrite(f'..//tmp//grid_cell_{x}_{y}___{w}_{h}.jpg', roi)
+    image_res = img.copy()
+    images_res = {}
 
-
-    # cv2.imshow('Result', img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    for key in cut_info.keys():
+        x, y, w, h = cut_info[key]
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        roi = image_res[y:y + h, x:x + w]
+        images_res[key] = roi
+    return images_res, img
 
 
 
@@ -166,7 +213,7 @@ def test3():
     lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=100, maxLineGap=20)
     slopes = []
     #自己定义一个存储线段的元祖list
-    line_info = []
+    line_info = {}
 
     for line in lines:
         x1, y1, x2, y2 = line[0]
@@ -188,6 +235,7 @@ def test3():
         if abs(slope - most_common_slope_1) < 5 or abs(slope - most_common_slope_2) < 5:
             selected_lines.append((x1, y1, x2, y2))
            # cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
 
     #设置边距
     dis_to_edge = 50
@@ -290,26 +338,33 @@ def grid_graph_old(image_original):
 
 def compare_images(imageA, imageB):
     # 记录空白格部分的特征
+    # #存储空白格的图片，也就是存储一次imageB
+    # save_path='none_feature.jpg'
+    # cv2.imwrite(save_path,imageB)
 
     none_feature = cv2.imread('none_feature.jpg')
-    imageA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
-    imageB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+    imageA_ = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+    imageB_ = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
     none_feature=cv2.cvtColor(none_feature, cv2.COLOR_BGR2GRAY)
-    _, imageA = cv2.threshold(imageA, 127, 255, cv2.THRESH_BINARY)
-    _, imageB = cv2.threshold(imageB, 127, 255, cv2.THRESH_BINARY)
-    imageA = cv2.resize(imageA, (100, 100))
-    imageB = cv2.resize(imageB, (100, 100))
+    _, imageA_ = cv2.threshold(imageA_, 127, 255, cv2.THRESH_BINARY)
+    _, imageB_ = cv2.threshold(imageB_, 127, 255, cv2.THRESH_BINARY)
+    imageA_ = cv2.resize(imageA_, (100, 100))
+    imageB_ = cv2.resize(imageB_, (100, 100))
     none_feature = cv2.resize(none_feature, (100, 100))
-    #image_show(none_feature, imageB)
-    score=ssim(none_feature, imageB)
+    #image_show(none_feature, imageB_)
+    score=ssim(none_feature, imageB_)
     #print('ssim',score)
-    if score>0.6:
+    if score>0.8:
         print('空白格')
         return 0
 
     #同时显示两个图
+    imageA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+    imageB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+    imageA = cv2.resize(imageA, (100, 100))
+    imageB = cv2.resize(imageB, (100, 100))
     score = ssim(imageA, imageB)
-    #image_show(imageA, imageB)
+   # image_show(imageA, imageB)
     #print('之前分',score)
     score = (score+1)*50
     #print('之后分',score)
@@ -320,10 +375,75 @@ def compare_images(imageA, imageB):
     return score
 
 
-def grid_graph(image_original):
+
+def grid_graph_v2(image_original,x=0,y=0,weigh=0,high=0):
+    w, h = image_original.shape[1], image_original.shape[0]
+    image = image_original.copy()
+    if x!=0 and y!=0 and weigh!=0 and high!=0:
+        x1 = x
+        y1 = y
+        x2 = x + weigh
+        y2 = y
+        x3 = x
+        y3 = y + high
+        x4 = x + weigh
+        y4 = y + high
+    else:
+        x1 = 0
+        y1 = 0
+        x2 = w
+        y2 = 0
+        x3 = 0
+        y3 = h
+        x4 = w
+        y4 = h
+
+    x1, y1, x2, y2, x3, y3, x4, y4 = [int(x) for x in [x1, y1, x2, y2, x3, y3, x4, y4]]
+    cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    cv2.line(image, (x1, y1), (x3, y3), (0, 0, 255), 2)
+    cv2.line(image, (x2, y2), (x4, y4), (0, 0, 255), 2)
+    cv2.line(image, (x3, y3), (x4, y4), (0, 0, 255), 2)
+
+    w_inner_left = 0.03
+    h_inner_top = 0.02
+    w_inner_right = 0.03
+    h_inner_bottom = 0.02
+
+    # 每两行之间的间距
+    h_line_interval = 0.011
+
+
+    h_line_interval=int(h*h_line_interval)
+    x1=x1+(w*(w_inner_left))
+    y1 = y1+(h*(h_inner_top))
+    # 右上角的坐标
+    x2 =x2-(w*(w_inner_right))
+    y2 = y2+(h*(h_inner_top))
+    # 左下角的坐标
+    x3 = x3+(w*(w_inner_left))
+    y3 = y3-(h*(h_inner_bottom))
+    # 右下角的坐标
+    x4 = x4-(w*(w_inner_right))
+    y4 = y4-(h*(h_inner_bottom))
+    # 画线
+    x1, y1, x2, y2, x3, y3, x4, y4 = [int(x) for x in [x1, y1, x2, y2, x3, y3, x4, y4]]
+    cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    cv2.line(image, (x1, y1), (x3, y3), (0, 0, 255), 2)
+    cv2.line(image, (x2, y2), (x4, y4), (0, 0, 255), 2)
+    cv2.line(image, (x3, y3), (x4, y4), (0, 0, 255), 2)
+    _col=10
+    _row=10
+    w_line = int((x2 - x1) / _col)
+
+    #
+    # cv2.imshow('Result', image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+def grid_graph_old_shouxie(image_original):
 
     #获得图片的宽高
-    h,w,_=image_original.shape
+    w,h=image_original.shape[1],image_original.shape[0]
     image=image_original.copy()
     #
     #
@@ -386,6 +506,9 @@ def grid_graph(image_original):
     x4 = w*w_right
     y4 = h*h_bottom
     #测试的时候划线外圈
+
+    #
+
     x1, y1, x2, y2, x3, y3, x4, y4 = [int(x) for x in [x1, y1, x2, y2, x3, y3, x4, y4]]
     cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
     cv2.line(image, (x1, y1), (x3, y3), (0, 0, 255), 2)
@@ -393,22 +516,45 @@ def grid_graph(image_original):
     cv2.line(image, (x3, y3), (x4, y4), (0, 0, 255), 2)
 
 
-    h_line_interval=int(h*h_line_interval)
-    x1=w*w_left+(w*(w_inner_left))
-    y1 = h*h_top+(h*(h_inner_top))
-    # 右上角的坐标
-    x2 =w*w_right-(w*(w_inner_right))
-    y2 = h*h_top+(h*(h_inner_top))
-    # 左下角的坐标
-    x3 = w*w_left+(w*(w_inner_left))
-    y3 = h*h_bottom-(h*(h_inner_bottom))
-    # 右下角的坐标
-    x4 = w*w_right-(w*(w_inner_right))
-    y4 = h*h_bottom-(h*(h_inner_bottom))
+    # cv2.imshow('Result', image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    # h_line_interval=int(h*h_line_interval)
+    # x1=w*w_left+(w*(w_inner_left))
+    # y1 = h*h_top+(h*(h_inner_top))
+    # # 右上角的坐标
+    # x2 =w*w_right-(w*(w_inner_right))
+    # y2 = h*h_top+(h*(h_inner_top))
+    # # 左下角的坐标
+    # x3 = w*w_left+(w*(w_inner_left))
+    # y3 = h*h_bottom-(h*(h_inner_bottom))
+    # # 右下角的坐标
+    # x4 = w*w_right-(w*(w_inner_right))
+    # y4 = h*h_bottom-(h*(h_inner_bottom))
     # 画线
+
+
+    #根据识别后的inner处理
+    x1=x1+(w*(w_inner_left))
+    y1 = y1+(h*(h_inner_top))
+    # 右上角的坐标
+    x2 =x2-(w*(w_inner_right))
+    y2 = y2+(h*(h_inner_top))
+    # 左下角的坐标
+    x3 = x3+(w*(w_inner_left))
+    y3 = y3-(h*(h_inner_bottom))
+    # 右下角的坐标
+    x4 = x4-(w*(w_inner_right))
+    y4 = y4-(h*(h_inner_bottom))
+    # 画线
+
+
+
+
     x1, y1, x2, y2, x3, y3, x4, y4 = [int(x) for x in [x1, y1, x2, y2, x3, y3, x4, y4]]
 
-
+    print('a',x1,y1,x2,y2,x3,y3,x4,y4)
 
 
     #要在这个区域内进行切割,首先切成10行
@@ -416,11 +562,14 @@ def grid_graph(image_original):
     h_line=int(((y4-y1)-h_line_interval*(_row-1))/_row)
     #每行的宽度
     w_line=int((x2-x1)/_col)
+    print('h_line',h_line)
+    print('w_line',w_line)
     for i in range(1,11):
-        cv2.line(image, (x1, y1+(i-1)*h_line+(i-1)*h_line_interval), (x2, y1+(i-1)*h_line+(i-1)*h_line_interval), (0, 0, 255), 2)
-        cv2.line(image, (x1, y1+(i)*h_line+(i-1)*h_line_interval), (x2, y1+(i)*h_line+(i-1)*h_line_interval), (0, 0, 255), 2)
+        #print('i',i,x1, int(y1+(i-1)*h_line+(i-1)*h_line_interval), x2, int(y1+(i-1)*h_line+(i-1)*h_line_interval))
+        cv2.line(image, (x1, int(y1+(i-1)*h_line+(i-1)*h_line_interval)), (x2, int(y1+(i-1)*h_line+(i-1)*h_line_interval)), (0, 0, 255), 2)
+        cv2.line(image, (x1, int(y1+(i)*h_line+(i-1)*h_line_interval)), (x2, int(y1+(i)*h_line+(i-1)*h_line_interval)), (0, 0, 255), 2)
 
-        cv2.line(image, (x1+i*w_line, y1), (x1+i*w_line, y4), (0, 0, 255), 2)
+        cv2.line(image, (int(x1+i*w_line), y1), (int(x1+i*w_line), y4), (0, 0, 255), 2)
 
         # 最终将images的位置切成10*10的小图，并存储在对象返回，并记录每个图的位置序号
     image_res = image_original.copy()
@@ -428,18 +577,20 @@ def grid_graph(image_original):
     for i in range(_col):
         for j in range(_row):
             x = x1 + i * w_line
-            y = y1 + j * h_line+(j)*h_line_interval
-            w = w_line
-            h = h_line
-            roi = image_res[y:y + h, x:x + w]
+            y = int(y1 + j * h_line+(j)*h_line_interval)
+            w_tmp = w_line
+            h_tmp = h_line
+            print('w',x,y,int(y + h_tmp),int(x + w_tmp))
+            roi = image_res[y:int(y + h_tmp), x:int(x + w_tmp)]
             images_res[(i, j)] = roi  # i 是列，j是行
     return images_res, image
 def main(image_original):
     images_res,image_process = grid_graph(image_original)
     #印刷体和手写体对对应行数
-
     #显示一下images
-
+    cv2.imshow('Result', image_process)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
     print_hand_related=((0,1),(2,3),(4,5),(6,7),(8,9))
@@ -454,7 +605,12 @@ def main(image_original):
     for a,b in print_hand_related:
         for i in range(_col):
             #align_images(images_res[(i,a)], images_res[(i,b)])
-            score=compare_images(images_res[(i,a)],images_res[(i,b)])
+            try:
+                score=compare_images(images_res[(i,a)],images_res[(i,b)])
+            #捕获keyerror
+            except KeyError:
+                print('keyerror',i,a,b)
+                continue
 
             score_dict[(i,a,b)]=score
 
@@ -499,7 +655,10 @@ def align_images(img1, img2):
 
 if __name__ == '__main__':
     pic_name='bianzhuhao.jpg'
-    #pic_name = 'biaozhun3.jpg'
+    pic_name = 'biaozhun4.jpg'
+    pic_name='biaozhun3.jpg'
+    pic_name='new.png'
     image = cv2.imread(pic_name)
     res=main(image)
+
 
